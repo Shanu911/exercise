3c77fbfa84bd925227e1899953f51029fe151232
Shanu Biswas <76697973+Shanu911@users.noreply.github.com>
2021-11-11 23:06:34 +0530

    Add files via upload

---
+++ previous_yr_Q_solutions/f3.f95
@@ -0,0 +1,22 @@
+program q2
+    implicit none
+    integer :: i,n
+    real :: term=1.0, s = 1.0, x
+    read *, x ,n
+
+    !----e^(x)-----------
+    do i=1,n
+       term = term*x/float(i)
+       s = s + term
+    end do
+    print *,s
+
+    !----e^(-x)------------
+    s = 1.0
+    term = 1.0
+    do i=1,n
+       term = -term*x/float(i)
+       s = s + term
+    end do
+    print *,s
+end program
